ac912092dc5bf6c217fbe20252330cdef27cb488
Emil <codewithemil@gmail.com>
2024-06-03 00:57:08 -0600

    Optimized the image-rendering process. Changed the logo and converted all images to .png

--- scripts/arrangement.js
+++ scripts/arrangement.js
@@ -1,7 +1,7 @@
 let html = "";
 
 images.forEach((image) => {
-   html += `<img src="images/${image.src}" alt="${image.name}" class="img-${image.gridkey}">`;
+   html += `<img loading="lazy" src="images/${image.src}" alt="${image.name}" class="img-${image.gridkey}">`;
 })
 
 document.querySelector(".js-main").innerHTML = html;

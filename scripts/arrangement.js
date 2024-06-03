550aa8c0534f5bf47a30e067456db9ef6e4b492a
Emil <codewithemil@gmail.com>
2024-06-02 22:56:36 -0600

    Added shortcuts and made the code more readable

--- scripts/arrangement.js
+++ scripts/arrangement.js
@@ -4,22 +4,15 @@ let thirdColumn = "";
 let fourthColumn = "";
 
 images.forEach((image) => {
+   const img = `<img src="images/${image.src}" alt="${image.name}">`;
    if(image.column === 1) {
-      firstColumn += `
-      <img src="images/${image.src}" alt="${image.name}">
-      `;
+      firstColumn += img;
    } else if (image.column === 2) {
-      secondColumn += `
-      <img src="images/${image.src}" alt="${image.name}">
-      `;
+      secondColumn += img;
    } else if (image.column === 3) {
-      thirdColumn += `
-      <img src="images/${image.src}" alt="${image.name}">
-      `;
+      thirdColumn += img;
    } else {
-      fourthColumn += `
-      <img src="images/${image.src}" alt="${image.name}">
-      `;
+      fourthColumn += img;
    }
 })
 
--- scripts/loader.js
+++ scripts/loader.js
@@ -2,6 +2,9 @@ const loader = document.querySelector(".js-loader");
 
 window.addEventListener("load", () => {
    loader.classList.add("loader-hidden");
+   setTimeout(() => {
+      loader.classList.add("loader-display-none");
+   }, 500)
    loader.addEventListener("transitionend", () => {
       document.body.removeChild(loader);
    })

04dc26314332f566b2789613451d23316f7138c5
Emil <codewithemil@gmail.com>
2024-06-04 01:03:06 -0600

    Made the tag filter feature interactive through JavaScript

--- scripts/arrangement.js
+++ scripts/arrangement.js
@@ -1,36 +1,54 @@
 let html = "";
+const showAll = document.querySelector(".js-show-all");
 
 function display() {
    document.querySelector(".js-main").innerHTML = html;
 }
 
 function loadImages() {
+   html = "";
    images.forEach((image) => {
       html += `<img loading="lazy" src="images/${image.src}" alt="${image.name}" class="img-${image.gridkey}">`;
    });
+   display();
 }
 
-loadImages();
-display();
+function clearSelectedTags() {
+   showAll.classList.remove("tag-selected");
+   tagbar.forEach((tag) => {
+      tag.classList.remove("tag-selected");
+   });
+}
 
-document.querySelector(".js-show-all").addEventListener("click", () => {
-   html = "";
+showAll.classList.add("tag-selected");
+loadImages();
+
+showAll.addEventListener("click", () => {
+   clearSelectedTags();
+   showAll.classList.add("tag-selected");
    loadImages();
-   display();
 });
 
 // Filtering images through tags inside the <nav> element
 const tagbar = document.querySelectorAll(".js-tagbar-li");
 tagbar.forEach((li) => {
    li.addEventListener("click", () => {
-      html = "";
+      clearSelectedTags();
+      li.classList.add("tag-selected");
 
+      html = "";
+      let foundImages = false;
       images.forEach((image) => {
          if (image.tags === li.innerHTML.toLowerCase()) {
             html += `<img loading="lazy" src="images/${image.src}" alt="${image.name}" class="img-${image.gridkey}">`;
+            foundImages = true;
          }
       });
+      
+      if (!foundImages) {
+         html = "No results found... yet.";
+      }
 
       display();
-   })
+   });
 });

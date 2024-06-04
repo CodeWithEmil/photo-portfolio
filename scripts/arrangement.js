ecc31e052621a3a66402240c1ba6e91275a718fc
Emil <codewithemil@gmail.com>
2024-06-04 00:28:53 -0600

    Classified and arranged images through tags using JavaScript

--- scripts/arrangement.js
+++ scripts/arrangement.js
@@ -1,7 +1,36 @@
 let html = "";
 
-images.forEach((image) => {
-   html += `<img loading="lazy" src="images/${image.src}" alt="${image.name}" class="img-${image.gridkey}">`;
-})
+function display() {
+   document.querySelector(".js-main").innerHTML = html;
+}
 
-document.querySelector(".js-main").innerHTML = html;
+function loadImages() {
+   images.forEach((image) => {
+      html += `<img loading="lazy" src="images/${image.src}" alt="${image.name}" class="img-${image.gridkey}">`;
+   });
+}
+
+loadImages();
+display();
+
+document.querySelector(".js-show-all").addEventListener("click", () => {
+   html = "";
+   loadImages();
+   display();
+});
+
+// Filtering images through tags inside the <nav> element
+const tagbar = document.querySelectorAll(".js-tagbar-li");
+tagbar.forEach((li) => {
+   li.addEventListener("click", () => {
+      html = "";
+
+      images.forEach((image) => {
+         if (image.tags === li.innerHTML.toLowerCase()) {
+            html += `<img loading="lazy" src="images/${image.src}" alt="${image.name}" class="img-${image.gridkey}">`;
+         }
+      });
+
+      display();
+   })
+});
